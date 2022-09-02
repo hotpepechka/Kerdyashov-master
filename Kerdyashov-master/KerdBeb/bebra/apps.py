604669ceb44887f2b5eec2045632b3ccdf459a08
hotpepechka <kjhk3107@gmail.com>
2022-09-02 20:14:36 +0500

    bebra

---
+++ Kerdyashov-master/KerdBeb/bebra/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BebraConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bebra'
---
+++ Kerdyashov-master/KerdBeb/bebra/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+
+
+def homepage_view(request):
+    return HttpResponse('Hello, World!')
